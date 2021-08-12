72cdba5eaa97e9c81ad907a52e9b0028c7e078f1
trinm8 <tim.sanders@student.uantwerpen.be>
2021-08-12 11:23:06 +0200

    update

--- services/UI/project/__init__.py
+++ services/UI/project/__init__.py
@@ -1,9 +1,20 @@
 import os
 
-from flask import Flask
+from flask import Flask, abort, g, url_for
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_admin.menu import MenuLink
+from project.adminViews import ModelViewAuthorized, ModelViewAuthorizedSuperAdmin, ModelViewAuthorizedMatches
+from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy_session import flask_scoped_session
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.ext.declarative import declarative_base
 
+import requests
 
-#db = SQLAlchemy()
+db = SQLAlchemy()
+
 
 def create_app(script_info=None):
 
@@ -13,7 +24,70 @@ def create_app(script_info=None):
     app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
 
-    #db.init_app(app)
+    try:
+        admin = Admin(app, template_mode='bootstrap3')
+        matchesEngine = create_engine("postgres://postgres:postgres@matches-db:5432/matches_dev", pool_pre_ping=True)
+        teamsEngine = create_engine("postgres://postgres:postgres@teamsclubs-db:5432/teams_dev", pool_pre_ping=True)
+        userEngine = create_engine("postgres://postgres:postgres@users-db:5432/users_dev", pool_pre_ping=True)
+        refereeEngine = create_engine("postgres://postgres:postgres@referees-db:5432/referees_dev", pool_pre_ping=True)
+
+        meta_dataMatches = MetaData(bind=matchesEngine, reflect=True)
+        matches = Table('match', meta_dataMatches, autoload_with=matchesEngine)
+
+        meta_dataTeams = MetaData(bind=teamsEngine, reflect=True)
+        teams = Table('teams', meta_dataTeams, autoload_with=teamsEngine)
+        clubs = Table('clubs', meta_dataTeams, autoload_with=teamsEngine)
+        division = Table('divisions', meta_dataTeams, autoload_with=teamsEngine)
+
+        meta_dataUsers = MetaData(bind=userEngine, reflect=True)
+        users = Table('users', meta_dataUsers, autoload_with=userEngine)
+
+        meta_dataReferees = MetaData(bind=refereeEngine, reflect=True)
+        referees = Table('referees', meta_dataReferees, autoload_with=refereeEngine)
+
+        Base = declarative_base()
+        class Matches(Base):
+            __table__ = Table('match', meta_dataMatches, autoload_with=matchesEngine)
+
+        class Teams(Base):
+            __table__ = Table('teams', meta_dataTeams, autoload_with=teamsEngine)
+
+        class Clubs(Base):
+            __table__ = Table('clubs', meta_dataTeams, autoload_with=teamsEngine)
+
+        class Division(Base):
+            __table__ = Table('divisions', meta_dataTeams, autoload_with=teamsEngine)
+
+        class Users(Base):
+            __table__ = Table('users', meta_dataUsers, autoload_with=userEngine)
+
+        class Referees(Base):
+            __table__ = Table('referees', meta_dataReferees, autoload_with=refereeEngine)
+
+        session = sessionmaker(binds={
+            matches: matchesEngine,
+            teams: teamsEngine,
+            clubs: teamsEngine,
+            division: teamsEngine,
+            users: userEngine,
+            referees: refereeEngine
+        })
+
+        finalSession = flask_scoped_session(session, app)
+
+        admin.add_view(ModelViewAuthorizedMatches(Matches, finalSession))
+        admin.add_view(ModelViewAuthorized(Teams, finalSession, category="Teams"))
+        admin.add_view(ModelViewAuthorized(Clubs, finalSession, category="Teams"))
+        admin.add_view(ModelViewAuthorized(Division, finalSession, category="Teams"))
+        admin.add_view(ModelViewAuthorized(Referees, finalSession))
+        admin.add_view(ModelViewAuthorizedSuperAdmin(Users, finalSession))
+
+        admin.add_link(MenuLink(name="Home Page", category='', url='/'))
+    except Exception:
+        pass
+
+
+    db.init_app(app)
 
     from project.main import ui_blueprint
     app.register_blueprint(ui_blueprint)
@@ -23,4 +97,3 @@ def create_app(script_info=None):
         return {'app': app}
 
     return app
-
